b6f884291c069fe64d48f5013a056c4deb1eb2b8
dece1225 <dece1225@naver.com>
2022-04-13 09:32:33 +0900

    add init files

---
+++ projectGame/app/src/main/java/kr/ac/tukorea/ge/sgp01/s2016182009/projectgame/MainActivity.java
@@ -0,0 +1,14 @@
+package kr.ac.tukorea.ge.sgp01.s2016182009.projectgame;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
